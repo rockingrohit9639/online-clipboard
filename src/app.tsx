5538682b2a2d6c0c20d69a0dc4e923332b69be0d
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-11-18 23:31:39 +0100

    feat(ui): set shadcn

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function App() {
-  return <div>This is home</div>
+  return (
+    <div className="flex h-screen items-center justify-center text-4xl">
+      <Button>Home</Button>
+    </div>
+  )
 }
